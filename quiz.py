a095b4b3aed9e0f8464b48eecaf1e0a36d1acd02
Rasmus Tomasson <rasmus.tomasson@gmail.com>
2024-10-04 22:08:18 +0200

    wip quiz generation, quiz format, missed things

--- quiz.py
+++ quiz.py
@@ -1,9 +1,24 @@
+from typing import List
 
+class Question:
+    def __init__(self, question: str):
+        pass
 
 class Quiz:
 
-    def __init__(self):
+    def __init__(self, quiz_file_pth):
+        self.quesions = []
+        self.parse(quiz_file_pth=quiz_file_pth)
+
+    def parse(self, quiz_file_pth):
+        f = open(quiz_file_pth, 'r')
+        content = f.read()
+        f.close()
+
+        for question in content.split('\n\n'):
+            self.quesions.append(Question(question=question))
+
+    def generate(self):
         pass
 
     
-
